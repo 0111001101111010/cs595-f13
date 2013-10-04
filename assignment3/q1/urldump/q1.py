316285bac8a2e86063e96f3632621cb2f4a11347
Stanzheng <szhen002@odu.edu>
2013-10-03 23:19:02 -0400

    saving progress

--- assignment3/q1/urldump/q1.py
+++ assignment3/q1/urldump/q1.py
@@ -1,8 +1,9 @@
 
-
+import BeautifulSoup
 import pycurl
 import StringIO
 import html2text
+import BS4
 from HTMLParser import HTMLParser
 
 class MLStripper(HTMLParser):
@@ -20,19 +21,26 @@ def strip_tags(html):
     return s.get_data()
 
 
-fn = open("sample.txt","r")
+fn = open("final_uri_list.txt","r")
 for i, line in enumerate(fn):
-	f = open("sample%i.data" %i,'w')
-	c = pycurl.Curl()
-	c.setopt(pycurl.URL, line)
-	c.setopt(pycurl.HTTPHEADER, ["Accept:"])
-	b = StringIO.StringIO()
-	c.setopt(pycurl.WRITEFUNCTION, b.write)
-	c.setopt(pycurl.FOLLOWLOCATION, 1)
-	c.setopt(pycurl.MAXREDIRS, 5)
-	c.perform()
-	content = b.getvalue()
-	#u = unicode(b.getvalue())
-	content =  strip_tags(content)
-	f.write(content)
-	f.close()
+	try:
+		print i
+		f = open("rawHtml%i.data" %i,'w')
+		c = pycurl.Curl()
+		c.setopt(pycurl.URL, line)
+		c.setopt(pycurl.HTTPHEADER, ["Accept:"])
+		c.setopt(pycurl.SSL_VERIFYPEER,0)
+		c.setopt(pycurl.SSL_VERIFYHOST,0)
+		b = StringIO.StringIO()
+		c.setopt(pycurl.WRITEFUNCTION, b.write)
+		c.setopt(pycurl.FOLLOWLOCATION, 1)
+		c.setopt(pycurl.MAXREDIRS, 20)
+		c.setopt(pycurl.TIMEOUT, 120)
+		c.perform()
+		content = b.getvalue()
+		#u = unicode(b.getvalue())
+		#content =  strip_tags(content)
+		f.write(content)
+		f.close()
+	except:
+		pass
