2e80abe450c4a296ed316618d5e765b77ba7ad17
Stanzheng <szhen002@odu.edu>
2013-10-10 21:24:32 -0400

    saved progress

--- assignment4/q1/q1.py
+++ assignment4/q1/q1.py
@@ -3,25 +3,28 @@
 
 from bs4 import BeautifulSoup
 import urllib2
- 
-redditFile = urllib2.urlopen("http://stanzh.com")
-redditHtml = redditFile.read()
-redditFile.close()
- #downloads all the reference link
-soup = BeautifulSoup(redditHtml)
-redditAll = soup.find_all("a")
-for links in soup.find_all('a'):
-    print (links.get('href'))
 
-
-fn = open("../100url.list","r")
+fn = open("100url.list","r")
 for i, line in enumerate(fn):
 	print i
-	f = open("../100urls/%i.data" %i,'w')
-	
+	f = open("100urls/%i.data" %i,'w')
+	hdr = { 'User-Agent' : 'Firefox 25.0' }
+	req = urllib2.Request(line, headers=hdr)
+	redditFile = urllib2.urlopen(req)
+	redditHtml = redditFile.read()
+	redditFile.close()
+	 #downloads all the reference link
+	soup = BeautifulSoup(redditHtml)
+	redditAll = soup.find_all("a")
+	for links in soup.find_all('a'):
+		content = links.get('href')
+		print(content)
+		f.write(content+"\n")
+	    	#f.write("\n")
+			#f.close()	
 	#content =  bleach.clean(o.read())
 
-	f.write(content.encode('utf-8'))
-	f.close()
+	#f.write(content)
+
 
 
