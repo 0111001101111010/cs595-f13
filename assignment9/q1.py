34c327b864d48f5e904aa0c4870bc7a503eabfb4
stanzheng <dev.stanzheng@gmail.com>
2013-12-09 15:14:37 -0500

    in progress

--- assignment9/q1.py
+++ assignment9/q1.py
@@ -2,8 +2,24 @@
 # -*- coding: utf-8 -*- 
 #http://www.pythonforbeginners.com/python-on-the-web/using-feedparser-in-python/
 import feedparser 
+f = open('feeds.txt', 'w')
+e = feedparser.parse('http://www.rssreader.com/englishfeeds.xml')
+d = feedparser.parse('http://www.rssreader.com/dutchfeeds.xml')
 
-d = feedparser.parse('http://www.reddit.com/r/python/.rss')
 
+#the two feeds we need
+required = "http://f-measure.blogspot.com/"+"\n"
+required2 = "http://ws-dl.blogspot.com/"+"\n"
+f.write(required)
+f.write(required2)
+
+
+
+#english links
+for post in e.entries:
+     f.write((post.link + "\n").encode('utf-8'))
+
+
+#deutch links
 for post in d.entries:
-    print (post.title + ": " + post.link + "\n").encode('utf-8')
+    f.write((post.link + "\n").encode('utf-8'))
